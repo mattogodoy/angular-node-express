606e756bf09a47e5763714ce2ecbb629a8e14afe
Matias Godoy <matto@MacBook-Pro-de-Matias.local>
2014-07-31 00:58:04 +0200

    master: Commit inicial

---
+++ back/app.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express();
+
+// Habilitamos CORS (Cross-origin resource sharing)
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get("/", function(req, res) {
+  res.sendfile('index.html');
+});
+
+app.post("/getData", function(req, res) {
+  var profesores = [
+    {
+      nombre: "Marcos",
+      curso: "jQuery"
+    },
+    {
+      nombre: "Borja",
+      curso: "Angular JS"
+    },
+    {
+      nombre: "Arturo",
+      curso: "Promises"
+    },
+    {
+      nombre: "Matias",
+      curso: "Node JS"
+    }
+  ];
+
+  res.json(profesores);
+});
+
+var port = 5001;
+app.listen(port, function() {
+ console.log("Listening on " + port);
+});
